5ca00f40d1d4b9762daecfc660dbab2ecad97ed4
thejenniekim <jk@udacity.com>
2014-10-26 21:40:35 -0700

    defined variables

--- js/app.js
+++ js/app.js
@@ -3,7 +3,14 @@
 
 // Plan! 
 
+// want to select elements in the DOM 
+var taskInput; // element with ID of #new-task 
+var addButton; // first button on page 
+var incompleteTasksHolder; // ul with ID #incomplete-tasks 
+var completedTasksHolder; // #completed-tasks 
+
 // Add new tasks
+var addTask = function(){
 	// when button is pressed
 	// create new list item with text from #new-task:
 		// input (checkbox)
@@ -12,16 +19,39 @@
 		// button.edit
 		// button.delete
 		// each of these elements needs to be modified and appended
+};
 
 
 // Edit existing tasks 
-
+var editTask = function(){
+	// When Edit button is pressed
+		// IF the parent class is .editMode
+			// switch from .editMode
+			// label text becomes the input's value
+		// ELSE
+			// switch to .editMode
+			// input value becomes the label's text
+	// Toggle .editMode on the parent
+};
 
 // Delete tasks
+var deleteTask = function(){
+	// when Delete button is pressed, 
+		// remove parent list item from the unordered list (ul)
+};
 
 
 // Mark tasks complete  
+var taskCompleted = function(){
+	// when checkbox is ticked,
+		// append the task list item to #completed-tasks
+};
 
 
 // Mark tasks as incomplete 
+var taskIncomplete = function(){
+	// when checkbox is UN-ticked,
+		// append the task list item to #incomplete-tasks
+};
+
 
