fa4a8e4ae7b70828b6efc0e8eb6fc06913d84e77
Jennie Kim <jk@udacity.com>
2014-10-26 21:12:02 -0700

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,27 @@
+// Problem: UI doesn't provide desired results (nothing happens when you click things)
+// Solution: Add interactivity so user can manage their daily tasks 
+
+// Plan! 
+
+// Add new tasks
+	// when button is pressed
+	// create new list item with text from #new-task:
+		// input (checkbox)
+		// label
+		// input (text)
+		// button.edit
+		// button.delete
+		// each of these elements needs to be modified and appended
+
+
+// Edit existing tasks 
+
+
+// Delete tasks
+
+
+// Mark tasks complete  
+
+
+// Mark tasks as incomplete 
+
