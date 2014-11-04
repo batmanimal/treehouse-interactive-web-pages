9482293fc40fad06bc19ed529e062154c3783257
thejenniekim <jk@udacity.com>
2014-11-04 13:51:48 -0800

    new task li

--- js/app.js
+++ js/app.js
@@ -9,18 +9,39 @@ var addButton = document.getElementsByTagName('button')[0]; // first button on p
 var incompleteTasksHolder = document.getElementById('incomplete-tasks'); // ul with ID #incomplete-tasks 
 var completedTasksHolder = document.getElementById('completed-tasks'); // #completed-tasks 
 
+// new task li
+var createNewTaskElement = function(taskString) {
+	// create li
+	var listItem = document.createElement('li');
+
+	// input (checkbox)
+	var checkBox = document.createElement('input'); // checkbox
+	// label
+	var label = document.createElement('label');
+	// edit input (text)
+	var editInput = document.createElement('input');
+	// button.edit
+	var editButton = document.createElement('button');
+	// button.delete
+	var deleteButton = document.createElement('button');
+
+	// each of these elements needs to be modified and appended
+	listItem.appendChild(checkBox);
+	listItem.appendChild(label);
+	listItem.appendChild(editInput);
+	listItem.appendChild(editButton);
+	listItem.appendChild(deleteButton);
+
+	return listItem;
+}
+
 // Add new tasks
 var addTask = function(){ // these are all event handlers 
 	console.log("Add task..."); // just checking to see that this event handler function is triggered 
-	// when button is pressed
-	// create new list item with text from #new-task:
-
-	// input (checkbox)
-		// label
-		// input (text)
-		// button.edit
-		// button.delete
-		// each of these elements needs to be modified and appended
+	// create new list item and add it to ul:
+	var listItem = createNewTaskElement('some new task string');
+	// append listItem to incompleteTasksHolder
+	incompleteTasksHolder.appendChild(listItem);
 };
 
 
@@ -50,6 +71,8 @@ var taskCompleted = function(){
 	console.log("Task completed..."); // just checking to see that this function is triggered for the right eventHandler
 	// when checkbox is ticked,
 		// append the task list item to #completed-tasks
+	var listItem = this.parentNode;
+	completedTasksHolder.appendChild(listItem);
 };
 
 
@@ -58,6 +81,8 @@ var taskIncomplete = function(){
 	console.log("Task incomplete..."); // just checking to see that this function is triggered for the right eventHandler
 	// when checkbox is UN-ticked,
 		// append the task list item to #incomplete-tasks
+	var listItem = this.parentNode;
+	incompleteTasksHolder.appendChild(listItem);
 };
 
 // set click handler to the addTask function (wiring)
@@ -67,9 +92,9 @@ addButton.onclick = addTask; // the onclick set to addTask - does not call addTa
 var bindTaskEvents = function(taskListItem, checkBoxEventHandler) { // params: the list items of the ul, event handler of the checkbox
 	console.log("bind li events"); // log something out to show that this function ran in the loops
 	// select taskListItem's children
-	var checkBox = taskListItem.querySelector("input[type=checkbox]");
-	var editButton = taskListItem.querySelector("button.edit");
-	var deleteButton = taskListItem.querySelector("button.delete");
+	var checkBox = taskListItem.querySelector('input[type=checkbox]');
+	var editButton = taskListItem.querySelector('button.edit');
+	var deleteButton = taskListItem.querySelector('button.delete');
 	// bind editTask to edit button
 	editButton.onclick = editTask;
 	// bind deleteTask to delete button
